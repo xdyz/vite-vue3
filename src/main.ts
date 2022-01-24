3671929291a8f86d7b4c6ddfb21d9761abe05bf4
相得益张 <zhj778930960@outlook.com>
2022-01-24 11:40:03 +0800

    <feat> mount

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,21 @@ import AntDesignVue from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import store from './stores/index'
 
-
 // 创建主界面，并且绑定容器id  创建应用实例对象
 // Vue3
 const app = createApp(App)
+app.use(AntDesignVue)
+
+
+// 一定要在mount 之前 使用
+app.use(store)
 app.mount('#app')
 
 
+
 // 注入antd 组件
-app.use(AntDesignVue)
 
 // 注入状态管理库
-app.use(store)
 
 
 console.log(createApp(App));
